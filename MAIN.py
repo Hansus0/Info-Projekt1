cb510664236be18f2e2fdb4ed7d14c0501734b61
SuperDuperJo <jonas.l.roder@gmail.com>
2025-09-17 10:46:08 +0200

    Update MAIN.py
    Zombiegrün wurde angepasst

--- MAIN.py
+++ MAIN.py
@@ -341,7 +341,7 @@ class Zombie(pygame.sprite.Sprite):
     def __init__(self, x, y, width=PLAYER_WIDTH, height=PLAYER_HEIGHT):
         super().__init__()
         self.image = pygame.Surface((width, height))
-        self.image.fill((50, 150, 50))  # grünlicher Block als Platzhalter
+        self.image.fill((30, 120, 10))  # grünlicher Block als Platzhalter
         self.rect = self.image.get_rect(topleft=(x, y))
 
         self.speed = 1   # normale Geschwindigkeit (langsam)
@@ -1317,4 +1317,5 @@ if __name__ == "__main__":
     # main is defined without parameters; call it directly
 
     main()
+
 
